3effa98a8b99b03250d4edfd0f091b5c553fd667
JeffreyBerger <jeffrey.j.berger@gmail.com>
2013-08-25 15:34:45 +0000

    Clearing things..?

--- application/config/config.php
+++ application/config/config.php
@@ -273,7 +273,7 @@ $config['sess_time_to_update']	= 300;
 |
 */
 $config['cookie_prefix']	= "";
-$config['cookie_domain']	= "www.rezoundmusic.com";
+$config['cookie_domain']	= "rezoundmusic.com";
 $config['cookie_path']		= "/";
 $config['cookie_secure']	= FALSE;
 
