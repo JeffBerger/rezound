36b061e4787435983f8bf642ad246c393f677154
JeffreyBerger <jeffrey.j.berger@gmail.com>
2013-08-28 00:06:12 -0400

    git test

--- application/views/home/home_view.php
+++ application/views/home/home_view.php
@@ -4,7 +4,7 @@
 	<input type="hidden" id="verified" value="false">
 <?php endif;?>
 
-TEST TEST TEST GITPULL TESTyz
+TEST TEST TEST GITPULL TESTyzii
 
 <h1><i class="icon-group icon-large"></i> Bands</h1>
 
